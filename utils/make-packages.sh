c19735e682e8a68f9776fba07c2ca2c36bd3f8df
Connor Bell (Makeshift) <git@connor-bell.com>
2022-09-25 22:29:24 +0100

    Make chrome crx's too for github releases

--- utils/make-packages.sh
+++ utils/make-packages.sh
@@ -11,6 +11,8 @@ jq '.manifest_version = 3' manifest.json >manifest.json.tmp && mv manifest.json.
 # chrome store wants zips...
 crx pack --zip-output chrome.zip -p ~/.ssh/id_rsa
 mv chrome.zip "$wd"/chrome.zip
+crx pack -o chrome.crx -p ~/.ssh/id_rsa
+mv chrome.crx "$wd"/chrome.crx
 # Firefox doesn't currently support manifest v3
 jq '.manifest_version = 2' manifest.json >manifest.json.tmp && mv manifest.json.tmp manifest.json
 crx pack -o firefox.crx -p ~/.ssh/id_rsa
